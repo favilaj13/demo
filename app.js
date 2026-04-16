a760f760004a1beeafef33446a107e5acc30d276
favilaj13 <favilaj13@gmail.com>
2026-04-16 09:07:17 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 8080;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hola Mundo desde OpenShift 🚀\n');
+  res.end('Hola Mundo desde OpenShift222 🚀\n');
 });
 
 server.listen(port, hostname, () => {
